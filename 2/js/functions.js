6f2de7b966b254183dda15ce29aa42f2f799fc34
Keks <keks@htmlacademy.ru>
2024-02-20 10:53:47 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -5,10 +5,10 @@ const checkoutLengthStr = function(str, strLength) {
   return 'not a string';
 };
 
-console.log(checkoutLengthStr('проверяемая строка',20));
-console.log(checkoutLengthStr('проверяемая строка',18));
-console.log(checkoutLengthStr('проверяемая строка',10));
-console.log(checkoutLengthStr(126,20));
+checkoutLengthStr('проверяемая строка',20);
+checkoutLengthStr('проверяемая строка',18);
+checkoutLengthStr('проверяемая строка',10);
+checkoutLengthStr(126,20);
 
 const checkoutPalindtom = function(str) {
   if (typeof str === 'string') {
@@ -23,10 +23,10 @@ const checkoutPalindtom = function(str) {
 };
 
 
-console.log(checkoutPalindtom('топот'));
-console.log(checkoutPalindtom('ДовОд'));
-console.log(checkoutPalindtom('Кекс'));
-console.log(checkoutPalindtom('Лёша на полке клопа нашёл '));
+checkoutPalindtom('топот');
+checkoutPalindtom('ДовОд');
+checkoutPalindtom('Кекс');
+checkoutPalindtom('Лёша на полке клопа нашёл ');
 
 const extractNamber = function(str) {
   if (typeof str === 'string') {
@@ -36,14 +36,14 @@ const extractNamber = function(str) {
         extractStr += str[i];
       }
     }
-    return parseInt(extractStr);
+    return parseInt(extractStr, 10);
   }
   return 'not a string';
-}
+};
 
-console.log(extractNamber('2023 год '));
-console.log(extractNamber('ECMAScript 2022'));
-console.log(extractNamber('1 кефир, 0.5 батона'));
-console.log(extractNamber('агент 007'));
-console.log(extractNamber('а я томат'));
+extractNamber('2023 год ');
+extractNamber('ECMAScript 2022');
+extractNamber('1 кефир, 0.5 батона');
+extractNamber('агент 007');
+extractNamber('а я томат');
 
